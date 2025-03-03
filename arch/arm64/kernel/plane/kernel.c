8c54067586773bdc74a0134d842d438e29016db2
Pei Meng <2398696309@qq.com>
2025-03-03 11:35:28 +0800

    feat: introduce plane mainloop

--- arch/arm64/kernel/plane/kernel.c
+++ arch/arm64/kernel/plane/kernel.c
@@ -4,6 +4,12 @@
 #include <asm/rsi.h>
 #include <asm/plane.h>
 
+static void plane_mainloop(void)
+{
+	for (;;) {
+	}
+}
+
 void plane_kernel_main(void)
 {
 	if (!is_realm_world())
@@ -12,4 +18,6 @@ void plane_kernel_main(void)
 	pr_info("P0: P0 World!\n");
 
 	plane_context_init();
+
+	plane_mainloop();
 }
